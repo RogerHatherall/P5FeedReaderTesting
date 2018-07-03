2e25ab4581522a2cdf670152b10b7952e6c460d1
Roger Hatherall <roger.hatherall@gmail.com>
2018-07-03 17:23:03 +0100

    Added test to check menu is hidden when page loads

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,18 +54,28 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
+    
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        
+        //When the hamburger button is clicked the menu-hidden class is toggled.
+        //The class will be in the body tag when the menu is hidden.
+        const $menuHidden = $('body').hasClass('menu-hidden');
+
+        it('is hidden', function() {
+            expect($menuHidden).toBeTruthy();
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
