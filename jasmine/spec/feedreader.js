55887203681d55dd6e02de190266b8f169600b0c
Roger Hatherall <roger.hatherall@gmail.com>
2018-07-04 16:27:37 +0100

    Added test of loadFeed function

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -95,6 +95,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -102,6 +103,18 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+            /*Call the loadFeed function */
+            beforeEach((done) => {
+                loadFeed(0, done);
+            });
+            
+            /*Check the length of the container */
+            it('has at least one feed ', (done) => {
+                expect($('.feed .entry').length).toBeGreaterThan(0);
+                done();
+            });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
